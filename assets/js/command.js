b0ca559805686e97e462c0ba8f800c1d8c5ba440
Iliya,Voropay <ivoropay8@gmail.com>
2018-03-10 21:33:22 +0200

    Firct Commit

---
+++ assets/js/command.js
@@ -0,0 +1,5 @@
+$('.left').slick(
+ dots: true,
+ prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-chevron-right"></i></button>',
+ nextArrow:'<button type="button" class="slick-next"><i class="fas fa-chevron-left"></i></button>',
+);
